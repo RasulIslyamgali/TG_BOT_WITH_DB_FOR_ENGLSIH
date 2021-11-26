955557603a9665c12c22fa7b3c4663d253de9f19
rasul_islyamgali <rasul.islyamgali@gmail.com>
2021-11-27 01:35:22 +0600

    test commit for .gitignore

--- manually_english_bot.py
+++ manually_english_bot.py
@@ -1,5 +1,5 @@
 import datetime
-
+import time
 from aiogram import Bot, Dispatcher, types, executor
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.dispatcher.filters.state import StatesGroup, State
